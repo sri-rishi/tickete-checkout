e0d6b23e850e7b02c52f0c972dad771ccc044cd9
sri-rishi <sririshi2612@gmail.com>
2024-02-06 00:24:11 +0530

    import and export component and style of booking summary

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
 import Button from "./button/Button";
 import Header from "./header/Header";
+import BookingSummary from "./bookingSummary/BookingSummary";
 
-export {Header, Button}
+export {
+    Header, 
+    Button, 
+    BookingSummary
+}
