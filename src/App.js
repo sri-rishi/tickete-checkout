c08ef710885326c9d69ef9b9c52c5892bafcc1ec
sri-rishi <sririshi2612@gmail.com>
2024-02-05 12:23:54 +0530

    feat: add header component to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { useEffect } from 'react';
 import './App.css';
+import { Header } from './components';
 
 function App() {
-  
+
   useEffect(() => {
     document.title = "tickete Assignment"
   }, [])
 
   return (
     <div className="App">
-      Hello
+      <Header />
     </div>
   );
 }
