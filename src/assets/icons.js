bb86c7f8f4195a815f30fc97cbd7befbd76586f7
sri-rishi <sririshi2612@gmail.com>
2024-02-06 00:22:24 +0530

    style: import icons and images for booking summary component

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,11 +1,24 @@
-import { IoIosArrowRoundBack } from "react-icons/io";
-import { IoMdLock } from "react-icons/io";
+import { IoIosArrowRoundBack, IoMdLock  } from "react-icons/io";
 import { HiOutlineQuestionMarkCircle } from "react-icons/hi2";
-
+import { PiTrophyFill , PiTicketFill, PiPiggyBankFill} from "react-icons/pi";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { BsCalendar2Fill , BsPlus, BsExclamationCircle} from "react-icons/bs";
+import { IoTime } from "react-icons/io5";
+import { MdPeopleAlt } from "react-icons/md";
 
 
 export {
     IoIosArrowRoundBack,
     IoMdLock,
-    HiOutlineQuestionMarkCircle
+    HiOutlineQuestionMarkCircle,
+    PiTrophyFill,
+    FaChevronLeft,
+    FaChevronRight,
+    PiTicketFill,
+    BsCalendar2Fill,
+    IoTime,
+    MdPeopleAlt,
+    BsPlus,
+    PiPiggyBankFill,
+    BsExclamationCircle
 }
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,41 @@
-import {IoIosArrowRoundBack, IoMdLock, HiOutlineQuestionMarkCircle} from "./icons";
+import {
+    IoIosArrowRoundBack, 
+    IoMdLock, 
+    HiOutlineQuestionMarkCircle, 
+    PiTrophyFill,
+    FaChevronLeft,
+    FaChevronRight,
+    PiTicketFill,
+    BsCalendar2Fill,
+    IoTime,
+    MdPeopleAlt,
+    BsPlus,
+    PiPiggyBankFill,
+    BsExclamationCircle
+} from "./icons";
 import LOGO from "./images/title-logo.png";
+import BOOKING_COVER_SM from "./images/booking-cover-sm.png";
+import BOOKING_COVER_LG from "./images/booking-cover-lg.png";
+import RATING_STAR from "./images/rating-star.png";
+import PAGINATION_INDICATOR from "./images/pagination-indicator.png";
 
 export {
     IoIosArrowRoundBack,
     IoMdLock,
     HiOutlineQuestionMarkCircle,
-    LOGO
+    PiTrophyFill,
+    FaChevronLeft,
+    FaChevronRight,
+    PiTicketFill,
+    BsCalendar2Fill,
+    IoTime,
+    MdPeopleAlt,
+    BsPlus,
+    PiPiggyBankFill,
+    BsExclamationCircle,
+    PAGINATION_INDICATOR,
+    LOGO,
+    BOOKING_COVER_SM,
+    BOOKING_COVER_LG,
+    RATING_STAR
 }
